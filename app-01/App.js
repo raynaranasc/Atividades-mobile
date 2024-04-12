e2be14b1b2ad574704965cde4b698e2f5487f0cf
Raynara Nascimento <90811047+raynaranasc@users.noreply.github.com>
2024-04-12 10:43:43 -0300

    Add files via upload

---
+++ app-01/App.js
@@ -0,0 +1,20 @@
+import { View, Text, Image} from 'react-native';
+
+
+import Componente1 from './src/componente1'
+import Componente2 from './src/componente2'
+
+
+const App = () => {
+  return (
+    <View>
+      <Componente1 />
+
+
+      <Componente2 />
+    </View>
+  );
+};
+
+
+export default App;
